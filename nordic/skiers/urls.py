70a21f42150cb1a78b86e5bef74ebe170687cd5b
Mipppy <122882588+Mipppy@users.noreply.github.com>
2024-02-11 17:07:40 -0500

    backup

--- nordic/skiers/urls.py
+++ nordic/skiers/urls.py
@@ -5,5 +5,6 @@ from . import views
 urlpatterns = [
     path("", views.index, name="index"),
     path("rescan", views.rescan, name="rescan"),
+    path("racers/<int:id>", views.racer, name="racers")
 ]
 
--- nordic/skiers/views.py
+++ nordic/skiers/views.py
@@ -5,11 +5,25 @@ from django.urls import reverse
 
 
 def index(request):
-    results = Sites.objects.all()
-    return render(request, "skiers/index.html", {"results": results})
+    races = Sites.objects.all()
+    racers = []
+    for result in Result.objects.all():
+        if result.place == 1:
+            result.racer.team = " ".join(str(result.racer.team).split(" ")[1:])
+            racers.append(result)
+    racers = racers[:20]
+    return render(request, "skiers/index.html", {"races": races, "racers": racers})
 
 def rescan(request):
     hrefs = mainPDFFunc()
     allPDFdata = PDFListedData(hrefs)
     parseRacerData(allPDFdata)
     return HttpResponseRedirect(reverse("index"))
+
+def racer(request, id):
+    racer = Racer.objects.get(id=id)
+    results = Result.objects.filter(racer=racer)
+    racer.team = " ".join(str(racer.team).split(" ")[1:])
+    racer.lastname = racer.lastname.lower().capitalize()[:-1]
+    return render(request, "skiers/racer.html", {"racer": racer, "results": results})
+
